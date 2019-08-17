e59872e9e6db3eb383e180af5534d841d34ce5ee
muqadusazam <30953671+muqadusazam@users.noreply.github.com>
2019-08-18 09:45:12 +1000

    Update RegisterServlet.java
    kelvin's register serlet fixed

--- src/main/java/fc/controller/RegisterServlet.java
+++ src/main/java/fc/controller/RegisterServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -42,21 +45,6 @@ public class RegisterServlet extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -68,24 +56,59 @@ public class RegisterServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        Map<String,String> errors = new HashMap<String,String>();
         
+        processRequest(request, response);
         HttpSession session = request.getSession();
+        
         String firstName = request.getParameter("fname");
+        if (!validate(namePattern, firstName)){
+            errors.put(firstName, "Incorrect name format");
+        }
+        
         String lastName = request.getParameter("lname");
+        if (!validate(namePattern, lastName)){
+            errors.put(lastName, "Incorrect name format");
+        }
+        
         String email = request.getParameter("email");
+        if (!validate(emailPattern, email)){
+            errors.put(email, "Incorrect email format");
+        }
+        
         String password = request.getParameter("password");
+        if (!validate(passwordPattern, password)){
+            errors.put(password, "Incorrect password format");
+        }
+        
+        String passport = request.getParameter("passport");
+        if (!validate(passportPattern, passport)){
+            errors.put(passport, "Incorrect passport format");
+        }
+        
         String DOB = request.getParameter("DOB");
+        
+        if (errors.isEmpty()){
+            //redirect to next page if no error
+            //response.sendRedirect(link);
+        }
+        else {
+            //put errors in request scope and forward them back to register.jsp
+            //request.setAttribute("errors", errors);
+            //request.getRequestDispatcher("register.jsp").forward(request, response);
+        }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    //regex for validating input from register.jsp form
+    private String emailPattern = "([a-zA-Z0-9]+)(([._-])([a-zA-Z0-9]+))*(@)([a-z]+)(.)([a-z]{3})((([.])[a-z]{0,2})*)";   
+    private String namePattern = "[A-Z]{1}[a-z]*"; //1 capital char, 0 or more lowercase chars
+    private String passwordPattern = "[a-z]{5,}[0-9]+"; //5 or more chars, 1 or more ints
+    private String passportPattern = "[A-Z]{2}[0-9]{3}"; //2 capital chars, 3 ints
+   
+    public boolean validate(String pattern, String input){ //return false if input does not match pattern
+        Pattern regEx = Pattern.compile(pattern);
+        Matcher match = regEx.matcher(input);
+        return match.matches();
+    }
+    
 }

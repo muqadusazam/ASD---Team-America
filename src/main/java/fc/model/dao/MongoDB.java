dfc4f51196b305c47ecfd81643f4cc8bc45b6795
muqadusazam <30953671+muqadusazam@users.noreply.github.com>
2019-08-30 17:42:55 +1000

    Create MongoDB.java

---
+++ src/main/java/fc/model/dao/MongoDB.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fc.model.dao;
+
+/**
+ *
+ * @author Liam
+ */
+public abstract class MongoDB {
+    protected final String OWNER = "admin";
+    protected final String PASSWORD = "qwerty123";
+    protected final String KEY = "@ds263927.mlab.com:63927/heroku_dlsrflnl"; 
+    
+    protected final String CUSTOMER_COLLECTION = "ASD.Customers(MUQADUS)";
+    protected final String TICKET_COLLECTION   = "ASD.Tickets(MUQADUS)";
+    protected final String FLIGHT_COLLECTION  = "ASD.Flights(MUQADUS)";
+}

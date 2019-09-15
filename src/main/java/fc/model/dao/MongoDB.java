989379788f7d67d771639687c95c7949f1c7f17b
muqadusazam <30953671+muqadusazam@users.noreply.github.com>
2019-09-15 18:30:41 +1000

    fixed customers and tickets mongoDB

--- src/main/java/fc/model/dao/MongoDB.java
+++ src/main/java/fc/model/dao/MongoDB.java
@@ -14,7 +14,7 @@ public abstract class MongoDB {
     protected final String PASSWORD = "qwerty123";
     protected final String KEY = "@ds263927.mlab.com:63927/heroku_dlsrflnl"; 
     
-    protected final String CUSTOMER_COLLECTION = "ASD.Customers()";
-    protected final String TICKET_COLLECTION   = "ASD.Tickets()";
+    protected final String CUSTOMER_COLLECTION = "ASD.Customers";
+    protected final String TICKET_COLLECTION   = "ASD.Tickets";
     protected final String FLIGHT_COLLECTION  = "ASD.Flights(Anthea)";
 }

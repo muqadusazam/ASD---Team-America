a0169d478365ec40f017bd8e7fa219be4bc097d9
muqadusazam <30953671+muqadusazam@users.noreply.github.com>
2019-09-16 01:40:36 +1000

    fixed mongoDB from ticket to Liam's

--- src/main/java/fc/model/dao/MongoDB.java
+++ src/main/java/fc/model/dao/MongoDB.java
@@ -15,6 +15,6 @@ public abstract class MongoDB {
     protected final String KEY = "@ds263927.mlab.com:63927/heroku_dlsrflnl"; 
     
     protected final String CUSTOMER_COLLECTION = "ASD.Customers";
-    protected final String TICKET_COLLECTION   = "ASD.Tickets";
+    protected final String TICKET_COLLECTION   = "ASD.Tickets(LIAM)";
     protected final String FLIGHT_COLLECTION  = "ASD.Flights(Anthea)";
 }

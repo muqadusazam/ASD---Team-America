c8e49eab984cbff7260b0648cd560faee895539a
muqadusazam <30953671+muqadusazam@users.noreply.github.com>
2019-10-21 17:07:21 +1100

    Update cancelTicketATDD.java

--- src/test/java/test/cancelTicketATDD.java
+++ src/test/java/test/cancelTicketATDD.java
@@ -17,8 +17,8 @@ public class cancelTicketATDD {
     WebDriver driver;
     
     //Login to Flight Centre
-    @Given("^I log in as \"([^\"]*)\" and \"([^\"]*)\"$")
-    public void i_log_in_as(String arg1, String arg2) throws Throwable{
+    @Given("^I logg in as \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void i_logg_in_as(String arg1, String arg2) throws Throwable{
         Class<? extends WebDriver> driverClass = FirefoxDriver.class;
         WebDriverManager.getInstance(driverClass).setup();
         driver = driverClass.newInstance();

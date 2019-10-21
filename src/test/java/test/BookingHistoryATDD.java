a86dc67864e366c367b335e2d40ed3ae0bb2afba
muqadusazam <30953671+muqadusazam@users.noreply.github.com>
2019-10-21 17:01:39 +1100

    Update BookingHistoryATDD.java

--- src/test/java/test/BookingHistoryATDD.java
+++ src/test/java/test/BookingHistoryATDD.java
@@ -20,16 +20,29 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class BookingHistoryATDD {
     WebDriver driver;
     
-        //Set starting page to bookingHistory.jsp
-    @Given("^I am on the booking history page$")
-    public void i_am_on_the_booking_history_page() throws Throwable {
+    @Given("^log in \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void log_in(String arg1, String arg2) throws Throwable{
         Class<? extends WebDriver> driverClass = FirefoxDriver.class;
         WebDriverManager.getInstance(driverClass).setup();
         driver = driverClass.newInstance();
-        driver.get("https://arsproject.herokuapp.com/booking_History.jsp");
+        driver.get("https://arsproject.herokuapp.com/login.jsp");
+        driver.navigate().to(driver.getCurrentUrl());
+        driver.findElement(By.id("Email")).sendKeys(arg1);
+        driver.findElement(By.id("Password")).sendKeys(arg2);
+        driver.findElement(By.id("BtnLogin")).click();
+        System.out.println("Given statement successful!");
+    }
+    
+    //Set starting page to bookingHistory.jsp
+    @Given("^I am on the booking history page$")
+    public void i_am_on_the_booking_history_page() throws Throwable {
+//        Class<? extends WebDriver> driverClass = FirefoxDriver.class;
+//        WebDriverManager.getInstance(driverClass).setup();
+//        driver = driverClass.newInstance();
+        driver.get("https://arsproject.herokuapp.com/booking_history.jsp");
         driver.navigate().to(driver.getCurrentUrl());
         System.out.println("Given statement executed successfully!");
-        throw new PendingException();
+       // throw new PendingException();
     }
     
         //Input query into departure textbox
@@ -37,7 +50,7 @@ public class BookingHistoryATDD {
     public void i_fill_in_departure_with(String arg1) throws Throwable {
         driver.findElement(By.id("departure")).sendKeys(arg1);
         driver.findElement(By.id("btnSubmit")).click();
-        throw new PendingException();
+      //  throw new PendingException();
     }
     
         //Search is successful
@@ -46,13 +59,13 @@ public class BookingHistoryATDD {
         System.out.println("Search successfull");
         driver.get("https://arsproject.herokuapp.com/ViewBookingServlet?departure=London&destination=");
         driver.navigate().to(driver.getCurrentUrl());  
-        throw new PendingException();
+       // throw new PendingException();
     }
     
         //Unsuccessful search, input was bad
     @Then("^I should return to booking history page$")
      public void i_should_return_to_booking_history_page() throws Throwable {
         System.out.println("Search unsuccessful, input was incorrect format");
-        throw new PendingException();
+       // throw new PendingException();
     }
 }
